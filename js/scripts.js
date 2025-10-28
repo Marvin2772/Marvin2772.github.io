be9101724e1cb9752d15714f9f639f886845e741
Marvin2772 <azertacio.0127@gmail.com>
2025-10-28 15:23:49 -0500

    cambios nuevos 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-function mostrarMensaje() {
-    alert("¡Gracias por visitar mi página!");
-}
+// JS para interacción básica (puedes expandir según necesidades)
+document.querySelectorAll('.meme').forEach(img => {
+  img.addEventListener('click', () => {
+    alert('¡Viva el humor!');
+  });
+});
